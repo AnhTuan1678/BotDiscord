d3e9dc53b5b76448f4ef2ff9d405c1a73f7afabb
SakamakyIzayoi <21020397@vnu.edu.vn>
2024-02-22 23:50:14 +0700

    Refactor chat history handling and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import { Client, IntentsBitField } from 'discord.js';
 import openai from './config/OpenAI.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const history = []; // Lịch sử chat
+
+import { Client, IntentsBitField } from 'discord.js';
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -18,30 +20,23 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-  assistant.history.push({ role: 'user', content: message.content });
   if (message.author.bot) return; // Bỏ qua tin nhắn từ bot khác
-  assistant.reply(message);
-});
-
-client.login(process.env.DISCORD_BOT_TOKEN);
-
-const assistant = {
-  role: 'assistant',
-  history: [],
-  // Lấy phản hồi từ OpenAI
-  getResponse: async () => {
+  history.push({ role: 'user', content: message.content });
+  try {
     const completion = await openai.chat.completions.create({
-      messages: assistant.history,
+      // Lấy lịch sử chat của người dùng
+      messages: history,
       model: 'gpt-3.5-turbo',
       max_tokens: 4000,
       temperature: 0,
     });
-    return completion.choices[0].message.content;
-  },
-  // Trả lời tin nhắn
-  reply: async (message) => {
-    const completionText = await assistant.getResponse();
+
+    const completionText = completion.choices[0].message.content;
     message.reply(completionText);
-    assistant.history.push({ role: 'assistant', content: completionText });
-  },
-};
+    history.push({ role: 'assistant', content: completionText });
+  } catch (error) {
+    message.reply('❌Có lỗi xảy ra khi tương tác với OpenAI');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
